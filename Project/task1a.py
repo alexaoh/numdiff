1791fe8cd1fb600ef2e5f73fb4110708e917922c
Jim Totland <jimtotland@gmail.com>
2021-02-13 12:36:12 +0100

    implemented cont L2 norm and relative error

--- Project/task1a.py
+++ Project/task1a.py
@@ -1,6 +1,8 @@
 """Numerical solution using the given difference method in task 1a."""
 
 from scipy.sparse import spdiags # Make sparse matrices with scipy.
+from scipy.interpolate import interp1d
+from scipy.integrate import quad
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
@@ -11,7 +13,7 @@ def f(x):
     return np.cos(2*np.pi*x) + x
 
 
-def anal_solution(x, alpha, sigma):
+def anal_solution(x, alpha = 0, sigma = 0):
     """Analytical solution of the Possion equation with given Neumann BC."""
     return -1/(4*np.pi**2)*np.cos(2*np.pi*x) + 1/6*x**3 + (sigma - 1/2)*x + alpha + 1/(4*np.pi**2)
 
@@ -76,14 +78,42 @@ def e_l(U, u):
     # Decide which to use! (Perhaps ask if we are allowed to use functions from numpy in general). 
     return disc_l2_norm(u-U)/disc_l2_norm(u), la.norm(u-U)/la.norm(u)
 
+def cont_L2_norm(v, left, right):
+    """ Continuous L2 norm of v(x) between left and right. """
+    integrand = lambda x: v(x)**2
+    return np.sqrt(quad(integrand, left, right)[0])
+
+def e_L(U, u, left, right):
+    """Relative error e_L.
+    
+    U: Approximate numerical solution.
+    u: Analytical solution. 
+    """
+    f = lambda x : u(x, alpha, sigma) - U(x)
+    numer = cont_L2_norm(f, left, right)
+    denom = cont_L2_norm(u, left, right)
+
+    return numer/denom
+
+
+
+
+
+
 M = np.arange(2, 1012, 10, dtype = int)
 discrete_error = np.zeros(len(M))
 discrete_errorF = np.zeros(len(M))
+cont_error = np.zeros(len(M))
+
 for i, m in enumerate(M):
     x = np.linspace(0, 1, m+2)
     Usol = num_solution(x, M = m, alpha = alpha, sigma = sigma)
     analsol = anal_solution(x, alpha, sigma)
+
     discrete_error[i], discrete_errorF[i] = e_l(Usol, analsol)
+
+    interpU = interp1d(x, Usol, kind = 'cubic')
+    cont_error[i] = e_L(interpU, anal_solution, x[0], x[-1])
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -91,6 +121,7 @@ ax.set_xscale("log")
 ax.set_yscale("log")
 ax.plot(M, discrete_error, label="el", color = "red")
 ax.plot(M, discrete_errorF, label="F", color = "black", linestyle = "dotted")
+ax.plot(M, cont_error, label = "eL", color = "blue")
 plt.legend()
 plt.grid() # Looks like it decreases with two orders!
 plt.show() 
