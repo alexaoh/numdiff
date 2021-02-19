4198d0dd585f32afbb6398359d18fd32a63b42ed
josteinklogetvedt <jostein.klogetvedt@gmail.com>
2021-02-19 15:26:46 +0100

    Added num_sol_first_order

--- Project/task1d.py
+++ Project/task1d.py
@@ -4,25 +4,25 @@ import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
 
-
-def f(x,epsilon):
+epsilon = 0.01
+def f(x):
     """Right hand side of 1D Poisson equation."""
     return epsilon**(-2)*np.exp(-(1/epsilon)*(x-0.5)**2)*(4*x**2 - 4*x + 1 - 2*epsilon)
 
 
-def anal_solution(x, epsilon):
+def anal_solution(x):
     """Manufactured solution of the Poisson equation with Dirichlet BC"""
     return np.exp(-(1/epsilon)*(x-0.5)**2)
 
 
-def num_sol_second_order(x, M, epsilon): #This is a second order method, using central difference
+def num_sol_second_order(x, M): #This is a second order method, using central difference
     """Second order numerical solution of the Possion equation with Dirichlet BC. given by the manufactured solution"""
 
     h = 1/(M+1)
 
     #Construct Dirichlet boundary condition from manuactured solution
-    alpha = anal_solution(0,epsilon)
-    beta = anal_solution(1,epsilon)
+    alpha = anal_solution(x[0])
+    beta = anal_solution(x[-1])
     
     # Construct Ah. 
     data = np.array([np.full(M, 1), np.full(M, -2), np.full(M, 1)])
@@ -30,7 +30,7 @@ def num_sol_second_order(x, M, epsilon): #This is a second order method, using c
     Ah = spdiags(data, diags, M, M).toarray()*1/h**2
 
     # Construct f.
-    f_vec = np.full(M, f(x[1:-1],epsilon))
+    f_vec = np.full(M, f(x[1:-1]))
     f_vec[0] = f_vec[0] - alpha/h**2
     f_vec[-1] = f_vec[-1] - beta/h**2
 
@@ -45,24 +45,23 @@ def num_sol_second_order(x, M, epsilon): #This is a second order method, using c
     
     return Usol
 
-def num_sol_first_order(x,M,epsilon):
+def num_sol_first_order(x,M):
     """First order numerical solution of the Possion equation with Dirichlet B.C. given by the manufactured solution
         Using a forward difference scheme"""
     h = 1/(M+1)
 
     #Construct Dirichlet boundary condition from manuactured solution
-    alpha = anal_solution(0,epsilon)
-    beta = anal_solution(1,epsilon)
+    alpha = anal_solution(x[0])
+    beta = anal_solution(x[-1])
     
     # Construct Ah. 
     data = np.array([np.full(M, 1), np.full(M, -2), np.full(M, 1)])
-    diags = np.array([0, 1, 2])
+    diags = np.array([-1, 0, 1])
     Ah = spdiags(data, diags, M, M).toarray()*1/h**2
     
-    #are these the right boundary conditions? Here I have not used alpha at all(?) - seems wrong to me
-    f_vec = np.full(M,f(x[1:-1],epsilon))
-    f_vec[-2] = f_vec[-2] - beta/h**2
-    f_vec[-1] = f_vec[-1] + 2*beta/h**2
+    f_vec = np.full(M,f(x[:-2]))
+    f_vec[0] = f_vec[0]- alpha/h**2
+    f_vec[-1] = f_vec[-1] - beta/h**2
 
     # Solve linear system. 
     Usol = la.solve(Ah, f_vec)
@@ -74,18 +73,17 @@ def num_sol_first_order(x,M,epsilon):
     Usol = np.append(Usol,beta)
     return Usol
 
-#M = 40
-#x = np.linspace(0, 1, M+2) # Make 1D grid.
-epsilon = 0.01 #may consider using this as a global variable, so we dont have to use it as input everywhere
-'''
-Usol_1 = num_sol_first_order(x,M,epsilon)
-Usol_2 = num_sol_second_order(x,M,epsilon)
+M = 20
+x = np.linspace(0, 1, M+2) # Make 1D grid.
+
+Usol_1 = num_sol_first_order(x,M)
+Usol_2 = num_sol_second_order(x,M)
 plt.plot(x,Usol_1,label="num1")
 plt.plot(x,Usol_2,label="num2")
-plt.plot(x,anal_solution(x,epsilon),label="an")
+plt.plot(x,anal_solution(x),label="an")
 plt.legend()
 plt.show()
-'''
+
 ### Uniform mesh refinement UMR, here using disc_error
 M_list = np.linspace(20,1000,20,dtype=int)
 h_list = 1/(M_list+1)
@@ -94,9 +92,9 @@ e_1 = np.zeros(len(M_list))
 e_2 = np.zeros(len(M_list))
 for i in range(len(M_list)):
     x = np.linspace(0,1,M_list[i]+2)
-    u = anal_solution(x,epsilon)
-    e_1[i] = la.norm(num_sol_first_order(x,M_list[i],epsilon) - u)/la.norm(u)
-    e_2[i] = la.norm(num_sol_second_order(x,M_list[i],epsilon) - u)/la.norm(u)
+    u = anal_solution(x)
+    e_1[i] = la.norm(num_sol_first_order(x,M_list[i]) - u)/la.norm(u)
+    e_2[i] = la.norm(num_sol_second_order(x,M_list[i]) - u)/la.norm(u)
 
 plt.plot(M_list,e_1,label="l2_first")
 plt.plot(M_list,8*h_list,label='O(h)',linestyle='dashed')
@@ -137,10 +135,10 @@ def num_solution_four_point_stencil(x):
     diags = np.array([-2,-1, 0, 1])
     Ah = spdiags(data, diags, M, M).toarray()
     
-    alpha = anal_solution(0,epsilon)
-    beta = anal_solution(1,epsilon)
+    alpha = anal_solution(x[0])
+    beta = anal_solution(x[-1])
     
-    f_vec = np.full(M, f(x[1:-1],epsilon))
+    f_vec = np.full(M, f(x[1:-1]))
     f_vec[0] = f_vec[0] - b[0]*alpha
     #f_vec[1] = f_vec[1] - a[1]*alpha #Should this be here?
     f_vec[-1] = f_vec[-1] - c[-1]*beta
@@ -163,7 +161,7 @@ def AMR_average(x0,steps):  #using average
     X_M = [x0]
     for k in range(steps):
         
-        u = anal_solution(X_M[-1],epsilon)        
+        u = anal_solution(X_M[-1])        
         error = la.norm(Usol_M[-1] - u)
         disc_error[k] = error/la.norm(u) #relative error
         average_error = error/len(X_M[-1]) #is this the correct form?
@@ -178,7 +176,7 @@ def AMR_average(x0,steps):  #using average
         X_M.append(x)
         Usol_M.append(num_solution_four_point_stencil(x))
         
-    u = anal_solution(X_M[-1],epsilon)
+    u = anal_solution(X_M[-1])
     disc_error[-1] = la.norm(Usol_M[-1]-u)/la.norm(u)
     return Usol_M, X_M, disc_error
 
