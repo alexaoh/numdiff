945eaf4790da8a7a9c6cf041a46f19a084ed5783
Jim Totland <jimtotland@gmail.com>
2021-02-19 17:22:07 +0100

    added a method for saving the reference solution.

--- Project/task2a.py
+++ Project/task2a.py
@@ -5,6 +5,7 @@ from scipy.sparse import spdiags # Make sparse matrices with scipy.
 import numpy as np
 import numpy.linalg as la
 import math
+import pickle # To save the reference solution.
 
 def disc_l2_norm(V):
     """Discrete l2-norm of V."""
@@ -20,9 +21,7 @@ def e_l(U, u):
     return disc_l2_norm(u-U)/disc_l2_norm(u) # la.norm(u-U)/la.norm(u)
 
 initial = (lambda x: 2*np.pi*x - np.sin(2*np.pi*x))
-def init(x):
-    '''Returns u_t(x,0)'''
-    return 4*np.pi**2 * np.sin(2*np.pi * x)
+
 
 M = 4 # Internal points in x dimension.
 N = 500 # Internal points in t dimension.
@@ -64,7 +63,7 @@ def secondOrder(M, plot = True):
     Q[0, 0] = Q[-1, -1] = -1
     Q[0, 1] = Q[-1, -2] = 0
     Q[0, 2] = Q[-1,-3] = 1
-    print(Q)
+    #print(Q)
 
     xGrid = np.linspace(0, L, M + 1)
     h = xGrid[1]-xGrid[0]
@@ -92,24 +91,30 @@ def secondOrder(M, plot = True):
     return sol
 
 
+def saveRefSol(Mstar, filename):
+    '''Saves the reference solution to file.'''
+    refSol = secondOrder(Mstar, plot = False)
+    with open(filename, 'wb') as fi:
+        pickle.dump(refSol, fi)
 
-#secondOrder()
 
-def calcError():
-    Mstar = 1000
+
+def calcError(Mstar, filename):
+    '''Calculates the relative error with the reference solution'''
+    refSol = None
+    with open(filename, 'rb') as fi:
+        refSol = pickle.load(fi)
+
     Mlist = [i for i in range(10, 200)]
     errorList = []
- 
-    refSol = secondOrder(Mstar, plot = False)
-
     piecewise = lambda u, refSol, M, Mstar : [refSol[math.floor(i*Mstar/M)] for i in range(len(u))]
     for M in Mlist:
         sol = secondOrder(M, plot = False)
         u = sol[5,:]
         uStar = np.array(piecewise(u, refSol[5,:], M, Mstar))
-        print("Ustar")
-        print(uStar)
-        print(sol)
+        #print("Ustar")
+        #print(uStar)
+        #print(sol)
         err = e_l(u, uStar)
         errorList.append(err)
     
@@ -118,5 +123,11 @@ def calcError():
     plt.xscale("log")
     plt.show()
 
-calcError()
+
+Mstar = 10000
+filename = 'refSol.pk'
+
+saveRefSol(Mstar, filename) # Only needs to be run once, or if you change Mstar
+calcError(Mstar, filename)
+
 
