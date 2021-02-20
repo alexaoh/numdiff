cc64ebd444d3b69a0c123046f307af1e0e85ea07
Jim Totland <jimtotland@gmail.com>
2021-02-20 16:13:47 +0100

    computed a refSOl on 10000 grid points in x-dir

--- Project/task2a.py
+++ Project/task2a.py
@@ -52,7 +52,7 @@ def secondOrder(M, plot = True):
     ''' Use central differences + trapezoidal '''
 
     
-    N = 10 # Internal points in t dimension.
+    N = 1 # Internal points in t dimension.
     L = 1 # Length of rod. 
 
     # Construct Q
@@ -67,7 +67,7 @@ def secondOrder(M, plot = True):
 
     xGrid = np.linspace(0, L, M + 1)
     h = xGrid[1]-xGrid[0]
-    tGrid = np.linspace(0, 0.5, N+1) # t-axis. 
+    tGrid = np.linspace(0, 0.1, N+1) # t-axis. 
     V0 = [initial(x) for x in xGrid]
 
     sol = trapezoidal_method(V0, Q, t, h)
@@ -102,16 +102,17 @@ def saveRefSol(Mstar, filename):
 def calcError(Mstar, filename):
     '''Calculates the relative error with the reference solution'''
     refSol = None
+    refTime = 3 # Time step at which to calculate error.
     with open(filename, 'rb') as fi:
         refSol = pickle.load(fi)
 
-    Mlist = [i for i in range(10, 200)]
+    Mlist = [i for i in range(10, 1000)]
     errorList = []
     piecewise = lambda u, refSol, M, Mstar : [refSol[math.floor(i*Mstar/M)] for i in range(len(u))]
     for M in Mlist:
         sol = secondOrder(M, plot = False)
-        u = sol[5,:]
-        uStar = np.array(piecewise(u, refSol[5,:], M, Mstar))
+        u = sol[refTime,:]
+        uStar = np.array(piecewise(u, refSol[refTime,:], M, Mstar))
         #print("Ustar")
         #print(uStar)
         #print(sol)
@@ -127,7 +128,7 @@ def calcError(Mstar, filename):
 Mstar = 10000
 filename = 'refSol.pk'
 
-saveRefSol(Mstar, filename) # Only needs to be run once, or if you change Mstar
+#saveRefSol(Mstar, filename) # Only needs to be run once, or if you change Mstar
 calcError(Mstar, filename)
 
 
