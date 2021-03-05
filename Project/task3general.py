7e9461d9a7cf2e872f3e053509e672282b36bdc3
alexaoh <alexaoh@users.noreply.github.com>
2021-03-05 12:29:23 +0100

    Began working on making convergence plot (with uniform grid sol).

--- Project/task3general.py
+++ Project/task3general.py
@@ -4,6 +4,7 @@ Trying to generalize the methods used in task3.py, in order to plot convergence 
 """
 
 from task3 import *
+from task1a import *
 
 def num_solution_uniform_grid(M):
     """Numerical solution of 2D Laplace on a uniform grid. Takes M = number of internal points in x and y."""
@@ -61,5 +62,36 @@ def anal_solution(x, y):
     """Analytic solution to the 2D Laplace equation."""
     return (1/np.sinh(2*np.pi))*np.sinh(2*np.pi*y)*np.sin(2*np.pi*x)
 
-U, xv, yv = num_solution_uniform_grid(M = 9)
-plot3d_sol(U,xv, yv,  Uan=anal_solution)
+#U, xv, yv = num_solution_uniform_grid(M = 9)
+#plot3d_sol(U,xv, yv,  Uan=anal_solution)
+
+
+## Make convergence plots below. Dette må lages spesifikt for hver retning tenker jeg. 
+
+M = np.arange(3, 1012/5, 100, dtype = int)
+discrete_error = np.zeros(len(M))
+cont_error = np.zeros(len(M))
+
+for i, m in enumerate(M):
+    x = np.linspace(0, 1, m+2)
+    Usol, xv, yv = num_solution_uniform_grid(m)
+    analsol = anal_solution(xv, yv)
+
+    discrete_error[i] = e_l(Usol, analsol)
+
+    #interpU = interp1d(x, Usol, kind = 'cubic')
+    #cont_error[i] = e_L(interpU, anal_solution, x[0], x[-1]) # Denne e_L må nok lages spesifikt til denne oppgaven!
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.plot(M, discrete_error, label=r"$e^r_l$", color = "blue", linewidth = 3)
+#ax.plot(M, cont_error, label = r"$e^r_{L_2}$", color = "yellow", linestyle = "--", linewidth = 2)
+#ax.plot(M, (lambda x: 1/x**2)(M), label=r"$O$($h^2$)", color = "red", linewidth = 2)
+ax.set_ylabel(r"Error $e^r_{(\cdot)}$")
+ax.set_xlabel("Number of points M")
+plt.legend()
+plt.grid() 
+#plt.savefig("convergencePlotTask3.pdf")
+plt.show() 
