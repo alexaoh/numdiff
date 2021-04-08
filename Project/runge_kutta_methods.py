1b6a8bbbd1420f0a80948811171f76f23c4478f7
Jim Totland <jimtotland@gmail.com>
2021-04-08 20:57:36 +0200

    made some plotting functionality and added an x-argument in the methods

--- Project/runge_kutta_methods.py
+++ Project/runge_kutta_methods.py
@@ -28,12 +28,12 @@ def RK3_step(h, k, t_i,y, f):
     s3 = f(h, t_i+k, y - s1 + 2*s2)
     return y + (k/6)*(s1 + 4*s2 + s3)
 
-def RK4_step(h, k, t_i, y, f):
+def RK4_step(h, k, t_i, y, f, x):
     """One step in RK4-method.""" 
-    s1 = f(h,t_i, y)
-    s2 = f(h, t_i+k/2, y + (k/2)*s1)  
-    s3 = f(h, t_i+k/2, y + (k/2)*s2) 
-    s4 = f(h, t_i+k, y + k*s3)
+    s1 = f(h,t_i, y, x)
+    s2 = f(h, t_i+k/2, y + (k/2)*s1, x)  
+    s3 = f(h, t_i+k/2, y + (k/2)*s2, x) 
+    s4 = f(h, t_i+k, y + k*s3, x)
     return y + (k/6)*(s1 + 2*s2 + 2*s3 + s4)
 
 def runge_kutta(x, t, f, method = RK4_step):
@@ -57,14 +57,14 @@ def runge_kutta(x, t, f, method = RK4_step):
     Y[0, 0, :] = u_0(x)
     Y[0, 1, :] = u_1(x)
     for i in range(N):
-        Y[i+1,:,1:-1] = method(h, k, t[i], Y[i,:,1:-1], f) #Løser kun for de nodene innenfor kanten, setter inn B.C etterpå.
+        Y[i+1,:,1:-1] = method(h, k, t[i], Y[i,:,1:-1], f, x) #Løser kun for de nodene innenfor kanten, setter inn B.C etterpå.
     
     #Insert B.C
     Y[:, 0, 0] = f_1(x[0],t)
     Y[:, 0, -1] = f_2(x[-1],t)
     return Y[:,0,:]
 
-def F(h,t_i,y):
+def F(h,t_i,y, x):
     """Expression for derivatives of y-vector = [v_m,w_m] for 1 <= m <= M+1.
     Using analytic function at boundary as boundary conditions.
     """
@@ -79,30 +79,45 @@ def F(h,t_i,y):
 
 #Løsningen divergerer når M>N. Noe som er feil her.
 #Funker derfor ikke med t-refinement
-M = 200
-N = 1000
+'''
+M = 1000
+N = 5000
 T = 5
-x = np.linspace(-2,2,M+1)
+x = np.linspace(-10,10,M+1)
 t = np.linspace(0,T,N+1)
 
 U = runge_kutta(x, t, F, RK4_step)
 
-plt.plot(x,U[-1,:])
-plt.plot(x,analytical_solution(x,t[-1]))
-plt.show()
-
-'''
-M = np.array([8,16,32,64,128])
-err = np.zeros(len(M))
-for i,m in enumerate(M):
-    x = np.linspace(-2,2,m+2)
-    U = runge_kutta(x,t,F,RK2_step)
-    u = analytical_solution(x,t[-1])
-    err[i] = la.norm(U[-1,:]-u)/la.norm(u)
-
-plt.plot(M, err)
-plt.plot(M,(1/M)**(2),linestyle='--')
-plt.xscale('log')
-plt.yscale('log')
+plt.plot(x,U[-1,:], label = "Numerical")
+plt.plot(x,analytical_solution(x,t[-1]), label = "analytical", linestyle = "dashed")
+plt.legend()
 plt.show()
 '''
+def plot_order(Ndof, error_start, order, label, color):
+    """Plots Ndof^{-order} from a starting point."""
+    const = (error_start)**(1/order)*Ndof[0]
+    plt.plot(Ndof, (const*1/Ndof)**order, label=label, color=color, linestyle='dashed')
+
+def h_refinement(method):
+    """Preforms t-refinement and plots the result."""
+    M = np.array([8,16,32,64,128])
+    err = np.zeros(len(M))
+    T = 5
+    N = 1000
+    t = np.linspace(0, T, N + 1)
+    for i,m in enumerate(M):
+        x = np.linspace(-2,2,m+2) 
+        U = runge_kutta(x,t,F,method)
+        u = analytical_solution(x,t[-1])
+        err[i] = la.norm(U[-1,:]-u)/la.norm(u)
+
+    plt.plot(M, err, marker = 'o', label = "$||u - U||_{L_2}$")
+    plot_order(M, err[0], 2, "$O(M^{-2})$", "red")
+    plt.xlabel("$M$")
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.legend()
+    plt.show()
+
+h_refinement(RK4_step)
+
