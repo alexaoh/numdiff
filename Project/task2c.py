cbcd91d9303653412426063c06b2e0b63d890b6c
josteinklogetvedt <jostein.klogetvedt@gmail.com>
2021-04-13 14:17:01 +0200

    Added old code (Jim's) for 2c back in branch

--- Project/task2c.py
+++ Project/task2c.py
@@ -1,41 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import RK45
-#from integrators import RK4_step
 
 def initial(x):
     return np.exp(-400*(x - 1/2)**2)
 
-def RK4_step(k, t_i, y, f):  #<--- This function can be imported from integrators.py after we merge the files from Part 2
-    '''Used as method in numerical_solution''' 
-    s1 = f(t_i, y)
-    s2 = f(t_i + k/2, y + (k / 2) * s1)  
-    s3 = f(t_i + k/2, y + (k / 2) * s2) 
-    s4 = f(t_i + k, y + k * s3) 
-    return y + (k / 6) * (s1 + (2 * s2) + (2 * s3) + s4)
 
-def numSol(x, t, method):   #Obs, initial conditions does not fit boundary conditions perfectly
+def RK4_step(v, k, f, t_i, M, h):
+    '''Used as method in numerical_solution''' 
+    s1 = f(t_i, v)
+    s2 = f(t_i + k/2, v + (k / 2) * s1)  
+    s3 = f(t_i + k/2, v + (k / 2) * s2) 
+    s4 = f(t_i + k, v + k * s3) 
+    return v + (k / 6) * (s1 + (2 * s2) + (2 * s3) + s4)
+
+def numSol(F, v0, tGrid, h, M, method): 
     '''Solves the ODE \dot{v} = f(t,v) with a specified method'''
-    M = len(x)-2
-    N = len(t)-1
-    h = x[1] - x[0]
-    k = t[1] - t[0]
+    N = len(tGrid)
+    k = tGrid[1] - tGrid[0]
+    vList = np.zeros((N,M))
+    vList[0,:] = v0
     
-    vList = np.zeros((N+1,M+2))
-    
-    #Solves for internal grid points
-    vList[0,1:-1] = initial(x[1:-1])
-    F = lambda t, v : np.concatenate(([-v[0]*v[1]], -v[1:-1]*(v[2:] - v[0:-2]), [v[-1]*v[-2]]))/(2*h)
-    
-    for i in range(N):
-        vList[i+1,1:-1] = method(k, t[i], vList[i,1:-1], F)  
-    
-    #Add B.C
-    zeros = np.zeros_like(t)
-    vList[:,0] = vList[:,-1] = zeros
+    for i in range(N-1):
+        val = method(vList[i,:], k, F, tGrid[i], M, h)  
+
+        vList[i+1,:] = val       
+
     return vList
 
-def plotTail(n, interval, sol, xGrid, tGrid):
+def F(t_i, v):
+    result = np.zeros(M)
+    result[0] = - v[1]*v[2]
+    result[-1] = v[-1]*v[-2]
+
+    for i in range(1, M - 1):
+        result[i] = (-v[i] * (v[i + 1] - v[i - 1]))
+    
+    return 1/(2*h) * result
+
+
+def plotTail(n, interval, sol, tGrid, xGrid):
     '''Plots the n last iterations of the solution'''
     for i in range(1, n*interval, interval):
         plt.plot(xGrid, sol[-i, :], label = f"$t = {tGrid[-i]}$")
@@ -43,47 +47,46 @@ def plotTail(n, interval, sol, xGrid, tGrid):
     plt.xlabel('$x$')
     plt.ylabel('$u(x,t)$')
     plt.legend()
-    plt.show()
+    #plt.show()
 
-# Plot breaking point of solution
+
 M = 1000
 N = 1000
-T = 0.06
-x = np.linspace(0, 1, M+2)
-t = np.linspace(0, T, N+1)
+xGrid = np.linspace(0, 1, M + 2)
+h = xGrid[1] - xGrid[0]
+tGrid = np.linspace(0,0.060, N)
 
-sol = numSol(x, t, RK4_step)
+v0 = np.array([initial(x) for x in xGrid[1:-1]])
 
-plotTail(4, 50, sol, x, t)
 
+sol = numSol(F, v0, tGrid, h, M, RK4_step)
+
+# Adding endpoints
+zeros = np.zeros((N,1))
+sol = np.hstack((zeros, sol))
+sol = np.hstack((sol, zeros))
+
+
+plotTail(4, 50, sol, tGrid, xGrid)
 
 
 
 # Solve with scipy:
 # Looks about right.
-def scipy_solution(x,t0,t_bound):
-    
-    h = x[1] - x[0]
-    F = lambda t, v : np.concatenate(([-v[0]*v[1]], -v[1:-1]*(v[2:] - v[0:-2]), [v[-1]*v[-2]]))/(2*h)
-    
-    scipySol = RK45(F, t0, initial(x[1:-1]), t_bound)
 
-    while scipySol.status != "finished":
-        scipySol.step()
+'''
+t_bound = tGrid[-1]
+scipySol = RK45(F, tGrid[0], v0, t_bound)
+while scipySol.status != "finished":
+    scipySol.step()
+y = scipySol.y
+zeros = np.zeros(1)
+y = np.hstack((zeros, y))
+y = np.hstack((y, zeros))
+plt.plot(xGrid, y, label = f"R45 t = {t_bound}", linestyle = "dotted")
+print(scipySol.t)
+print(scipySol.status)
+plt.legend()
+'''
 
-    y = scipySol.y
-    zeros = np.zeros(1)
-    y = np.hstack((zeros, y))
-    y = np.hstack((y, zeros))
-    plt.plot(x, y, label = f"R45 t = {t_bound}", linestyle = "dotted")
-    print(scipySol.t)
-    print(scipySol.status)
-
-    plt.legend()
-    plt.show()
-    
-#scipy_solution(x, 0, t[-1])
-
-
-
-
+plt.show()
