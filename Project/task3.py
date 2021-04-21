1aa068027ee40df319c6fa4d605d7e7bb190cdf3
alexaoh <47002854+alexaoh@users.noreply.github.com>
2021-04-21 17:22:25 +0200

    OOP version of task 3. (#24)
    * Commit working code before attempting OOP.
    
    * Works for "both", missing varying = Mx and varying = My.
    
    * Works for varying also now! Make final fixes next, then done.
    
    * Completed task3_OOP.
    
    * Fixed plots.
    
    * Changed constant values when making convergence plots.
    
    Co-authored-by: alexaoh <alexaoh@users.noreply.github.com>

--- Project/task3.py
+++ Project/task3.py
@@ -2,168 +2,141 @@
 
 h and k, i.e. step sizes in x and y respectively, DO NOT have to be equal. 
 """
-
 from scipy.sparse import diags # Make sparse matrices with scipy.
 import scipy.sparse.linalg
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
 from plotting_utilities import plot3d_sol
+from utilities import *
 
-def analytic_solution(x, y):
-    """Analytical solution to the 2D Laplace equation."""
-    return (1/np.sinh(2*np.pi))*np.sinh(2*np.pi*y)*np.sin(2*np.pi*x)
+class Task3:
+    def plot_solution(self, Mx = 50, My = 50):
+        """Calculate numerical solution on specified grid, and plot together with analytical solution."""
+        U, xv, yv = self.num_solution_Mx_My(Mx = Mx, My = My)
+        plot3d_sol(U, xv, yv, Uan = self.analytic_solution)
 
-def num_solution_Mx_My(Mx, My):
-    """Numerical solution of 2D Laplace.
-    
-    Input: 
-    Mx: Number of internal points in x-direction. 
-    My: Number of internal points in y-direction. 
-    """
-    M2 = Mx*My
-    h = 1/(Mx+1)
-    k = 1/(My+1)
+    def convergence_plot(self, varying, savename = False):
+        """Make convergence plot specified by which quantity is varying."""
+        assert(varying == "Mx" or varying == "My" or varying == "Both") 
+        self._colors = ["red", "green", "black", "orange"]
+        self._powers = [2] # Power used in the convergence plot. 
 
-    # Construct A. 
-    data = np.array([np.full(M2, 1/k**2)[:-1], np.full(M2, -2*(1/h**2+1/k**2)), np.full(M2, 1/k**2)[:-1], 
-                        np.full(M2, 1/h**2)[:-My], np.full(M2, 1/h**2)[:-My]])
-    diag = np.array([-1, 0, 1, -My, My])
-    A = diags(data, diag, format = "csc")
-    
-    # Construct F.
-    F = np.zeros(M2)
-    point_counter = 1
+        # Assert that the savename variable is of the correct format.
+        if (varying == "Mx" or varying == "My") and savename:
+            assert(type(savename) is list and len(savename) == 4)
+        elif savename:
+            assert(isinstance(savename, str))
 
-    # Change some elements to match the correct linear system + construct F. 
-    for i in range(My, My*Mx, My):
-        A[i-1, i] = A[i, i-1] = 0
-        F[i-1] = -(1/k**2)*np.sin(2*np.pi*point_counter*h)
-        point_counter += 1
+        if varying == "Mx":
+            self._constant_list = [10, 100, pow(10, 3), pow(10, 4)] # Constant values in plots. 
+            maximum = 2**7 # Maximum limit of Mx.
+        elif varying == "My":
+            self._constant_list = [10, 100, pow(10, 3), pow(10, 4)] # Constant values in plots. 
+            maximum = 2**7 # Maximum limit of My.
+        elif varying == "Both":
+            maximum = 2**10 # Maximum limit of My and Mx. 
+            self._powers = [1] # Power used in the convergence plot. 
 
-    # Add values out of loop-bound.  
-    F[M2-1] = -(1/k**2)*np.sin(2*np.pi*point_counter*h)
+        varying_list = 2 ** np.arange(1, np.log(maximum)/np.log(2)+1, dtype = int)
+        if varying == "Both":
+            self._discrete_error = np.zeros(len(varying_list))
+            for i, m in enumerate(varying_list):
+                Usol, xv, yv = self.num_solution_Mx_My(Mx = m, My = m)
+                analsol = self.analytic_solution(xv, yv)
+                self._discrete_error[i] = e_l(Usol, analsol)
+            if savename:
+                self.plot_plots(varying_list, varying_list, savename=savename)
+            else: 
+                self.plot_plots(varying_list, varying_list)
+        elif varying:
+            for j, constant in enumerate(self._constant_list):
+                self._discrete_error = np.zeros(len(varying_list))
+                for i, m in enumerate(varying_list):
+                    if varying == "Mx":
+                        Usol, xv, yv = self.num_solution_Mx_My(Mx = m, My = constant)
+                    elif varying == "My":
+                        Usol, xv, yv = self.num_solution_Mx_My(Mx = constant, My = m)
 
-    
-    # Solve linear system.  
-    Usol = scipy.sparse.linalg.spsolve(A, F) 
-    
-    # Next, want to unpack into grids, for plotting.
-    x = np.linspace(0, 1, Mx+2)
-    y = np.linspace(0, 1, My+2) 
+                    analsol = self.analytic_solution(xv, yv)
+                    self._discrete_error[i] = e_l(Usol, analsol)
+                if savename:
+                    self.plot_plots(varying_list, constant, savename=savename[j])
+                else: 
+                    self.plot_plots(varying_list, constant)
 
-    xv, yv = np.meshgrid(x, y)
-   
-    U = np.zeros_like(xv) # Grid for solution. Need to insert solution into this, including boundary conditions. 
+    def analytic_solution(self, x, y):
+        """Analytical solution to the 2D Laplace equation."""
+        return (1/np.sinh(2*np.pi))*np.sinh(2*np.pi*y)*np.sin(2*np.pi*x)
 
-    # Insert upper boundary condition last in U, since y increases "downwards" in yv. 
-    U[-1, :] = np.sin(2*np.pi*x)
-
-    # Need to unpack the solution vector with the correct coordinates. 
-    for i in range(int(len(Usol)/My)): # This gives the rows (x-values).
-        for j in range(My): # This gives the columns (y-values).
-            U[j+1, i+1] = Usol[j + (My*i)]
-
-    return U, xv, yv
-
-U, xv, yv = num_solution_Mx_My(Mx = 50, My = 50)
-plot3d_sol(U, xv, yv, Uan = analytic_solution)
-
-def constant_My_convergence_plot(My):
-    """Convergence plot with My kept constant and Mx increasing."""
-    maximum = 2**11
-    Mx = 2 ** np.arange(1, np.log(maximum)/np.log(2)+1, dtype = int)
-    discrete_error = np.zeros(len(Mx))
-    for i, m in enumerate(Mx):
-        Usol, xv, yv = num_solution_Mx_My(Mx = m, My = My)
-        analsol = analytic_solution(xv, yv)
-
-        discrete_error[i] = la.norm(analsol-Usol)/la.norm(analsol)
-
-    def plot_plots(savename = False):
-        """Encapsulation for development purposes."""
-        power = 1.5
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_xscale("log")
-        ax.set_yscale("log")
-        ax.plot(Mx*My, discrete_error, label=r"$e^r_\ell$", color = "blue", marker = "o", linewidth = 3)
-        ax.plot(Mx*My, (lambda x: 1/x**power)(Mx), label=r"$O$($h^{%s}$)" % str(power), color = "red", linewidth = 2)
-        ax.set_ylabel(r"Error $e^r_{(\cdot)}$")
-        ax.set_xlabel(r"$M_x \cdot M_y$")
-        fig.suptitle(r"$M_y = $"+str(My)+" constant")
-        plt.legend()
-        plt.grid() 
-        if savename:
-            plt.savefig(savename+".pdf")
-        plt.show() 
-
-    plot_plots()
-
-def constant_Mx_convergence_plot(Mx):
-    """Convergence plot with Mx kept constant and My increasing."""
-    maximum = 2**11
-    My = 2 ** np.arange(1, np.log(maximum)/np.log(2)+1, dtype = int)
-    discrete_error = np.zeros(len(My))
-    
-    for i, m in enumerate(My):
+    def num_solution_Mx_My(self, Mx, My):
+        """Numerical solution of 2D Laplace.
         
-        Usol, xv, yv = num_solution_Mx_My(Mx = Mx, My = m)
-        analsol = analytic_solution(xv, yv)
+        Input: 
+        Mx: Number of internal points in x-direction. 
+        My: Number of internal points in y-direction. 
+        """
+        M2 = Mx*My
+        h = 1/(Mx+1)
+        k = 1/(My+1)
 
-        discrete_error[i] = la.norm(analsol-Usol)/la.norm(analsol)
-
-    def plot_plots(savename = False):
-        """Encapsulation for development purposes."""
-        power = 1.5
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_xscale("log")
-        ax.set_yscale("log")
-        ax.plot(Mx*My, discrete_error, label=r"$e^r_\ell$", color = "blue", marker = "o", linewidth = 3)
-        ax.plot(Mx*My, (lambda x: 1/x**power)(My), label=r"$O$($h^{%s}$)" % str(power), color = "red", linewidth = 2)
-        ax.set_ylabel(r"Error $e^r_{(\cdot)}$")
-        ax.set_xlabel(r"$M_y \cdot M_y$")
-        fig.suptitle(r"$M_x = $"+str(Mx)+" constant")
-        plt.legend()
-        plt.grid() 
-        if savename:
-            plt.savefig(savename+".pdf")
-        plt.show() 
-
-    plot_plots()
-
-def convergence_plot_both_varying():
-    """Convergence plot with both Mx and My varying."""
-    maximum = 2**10
-    My = Mx = 2 ** np.arange(1, np.log(maximum)/np.log(2)+1, dtype = int)
-    discrete_error = np.zeros(len(My))
-    
-    for i in range(len(My)):
+        # Construct A. 
+        data = np.array([np.full(M2, 1/k**2)[:-1], np.full(M2, -2*(1/h**2+1/k**2)), np.full(M2, 1/k**2)[:-1], 
+                            np.full(M2, 1/h**2)[:-My], np.full(M2, 1/h**2)[:-My]])
+        diag = np.array([-1, 0, 1, -My, My])
+        A = diags(data, diag, format = "csc")
         
-        Usol, xv, yv = num_solution_Mx_My(Mx = Mx[i], My = My[i])
-        analsol = analytic_solution(xv, yv)
+        # Construct F.
+        F = np.zeros(M2)
+        point_counter = 1
 
-        discrete_error[i] = la.norm(analsol-Usol)/la.norm(analsol)
+        # Change some elements to match the correct linear system + construct F. 
+        for i in range(My, My*Mx, My):
+            A[i-1, i] = A[i, i-1] = 0
+            F[i-1] = -(1/k**2)*np.sin(2*np.pi*point_counter*h)
+            point_counter += 1
 
-    def plot_plots(savename = False):
-        """Encapsulation for development purposes."""
-        power = 1.5
-        power2 = 2
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_xscale("log")
-        ax.set_yscale("log")
-        ax.plot(Mx*My, discrete_error, label=r"$e^r_\ell$", color = "blue", marker = "o", linewidth = 3)
-        ax.plot(Mx*My, (lambda x: 1/x**power)(Mx), label=r"$O$($h^{%s}$)" % str(power), color = "red", linewidth = 2)
-        ax.plot(Mx*My, (lambda x: 1/x**power2)(Mx), label=r"$O$($h^{%s}$)" % str(power2), color = "green", linewidth = 2)
-        ax.set_ylabel(r"Error $e^r_{(\cdot)}$")
-        ax.set_xlabel(r"$M_y \cdot M_y$")
-        fig.suptitle(r"$M_x$"+" and "+r"$M_y$"+" varying")
-        plt.legend()
-        plt.grid() 
-        if savename:
-            plt.savefig(savename+".pdf")
-        plt.show() 
+        # Add values out of loop-bound.  
+        F[M2-1] = -(1/k**2)*np.sin(2*np.pi*point_counter*h)
 
-    plot_plots("task3bBothVary")
+        # Solve linear system.  
+        Usol = scipy.sparse.linalg.spsolve(A, F) 
+        
+        # Next, want to unpack into grids, for plotting.
+        x = np.linspace(0, 1, Mx+2)
+        y = np.linspace(0, 1, My+2) 
+        xv, yv = np.meshgrid(x, y)
+        U = np.zeros_like(xv) # Grid for solution. Need to insert solution into this, including boundary conditions. 
+
+        # Insert upper boundary condition last in U, since y increases "downwards" in yv. 
+        U[-1, :] = np.sin(2*np.pi*x)
+
+        # Need to unpack the solution vector with the correct coordinates. 
+        for i in range(int(len(Usol)/My)): # This gives the rows (x-values).
+            for j in range(My): # This gives the columns (y-values).
+                U[j+1, i+1] = Usol[j + (My*i)]
+
+        return U, xv, yv
+
+    def plot_plots(self, Mx, My, savename = False):
+            """Helper function used when plotting convergence plots."""
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.set_xscale("log")
+            ax.set_yscale("log")
+            ax.plot(Mx*My, self._discrete_error, label=r"$e^r_\ell$", color = "blue", marker = "o", linewidth = 3)
+            for i, p in enumerate(self._powers): # If one wants to plot several orders. 
+                plot_order(Mx*My, self._discrete_error[0], p, r"$\mathcal{O}(N_{dof}^{-%s})$)" % str(p), self._colors[i])
+            ax.set_ylabel(r"Error $e^r_{(\cdot)}$")
+            ax.set_xlabel(r"$M_x \cdot M_y$")
+            plt.legend()
+            plt.grid() 
+            if savename:
+                plt.savefig(savename+".pdf")
+            plt.show() 
+    
+solution = Task3()
+#solution.plot_solution()
+#solution.convergence_plot("Mx", savename=["task3bMy20", "task3bMy50", "task3bMy100", "task3bMy500"])
+#solution.convergence_plot("My", savename=["task3bMx20", "task3bMx50", "task3bMx100", "task3bMx500"])
+#solution.convergence_plot("Both", savename="task3bBothVary")
