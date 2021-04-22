d9870b34171febed2e4916e47bc0883ecf1035c6
Jim Totland <jimtotland@gmail.com>
2021-04-22 11:00:02 +0200

    added possibility for minus sign on analytical sol

--- Project/taskSG_d_and_e.py
+++ Project/taskSG_d_and_e.py
@@ -6,12 +6,13 @@ from scipy.sparse import spdiags
 from integrators import RK2_step, RK3_step, RK4_step, RKN12_step, RKN34_step
 from utilities import e_l, plot_order
 from plotting_utilities import plot3d_sol_part2
+import numpy.linalg as la
 
 c = 0.5  
 def analytical_solution(x,t):
     """Analytical solution, choosing c=1/2 and the plus-sign as solution."""
-    b = (x-c*t)*(1-c**2)**(-1/2)
-    return 4*np.arctan(np.exp(b))
+    b = (x - c*t)/np.sqrt(1 - c**2)
+    return 4*np.arctan(np.exp(-b))
 
 def num_solution(x, t, method):
     """Numeric solution using Runge-Kutta or Runge-Kutta-Nystrøm schemes, solved with 'method' step.
@@ -26,7 +27,10 @@ def num_solution(x, t, method):
     f_1 = lambda t : analytical_solution(x[0],t)
     f_2 = lambda t : analytical_solution(x[-1],t)
     u_0 = lambda x : analytical_solution(x,0)
-    u_1 = lambda x : -4*c*np.exp(x*(1-c**2)**(-1/2))/((1-c**2)**(1/2)*(1 + np.exp(2*x*(1-c**2)**(-1/2))))
+    #u_1 = lambda x : -4*c*np.exp(x*(1-c**2)**(-1/2))/((1-c**2)**(1/2)*(1 + np.exp(2*x*(1-c**2)**(-1/2))))
+    # With minus sign in the anal-sol.
+    u_1 = lambda x : 4*c*np.exp(-x/np.sqrt(1 - c**2)) / (( np.sqrt(1 - c**2) * (np.exp(-2*x/np.sqrt(1 - c**2)) + 1)))
+
     
     N = len(t)-1
     M = len(x)-2
@@ -72,12 +76,12 @@ M=20; N=10; T = 5
 x = np.linspace(-5,5,M+2)
 t = np.linspace(0,T,N+1)
 U = num_solution(x, t, RK4_step)
-plot3d_sol_part2(x,t,U,analytical_solution)
+#plot3d_sol_part2(x,t,U,analytical_solution)
 
 
 def refinement(M,N,method,savename=False):
     """Preforms h- or t-refinement and plots the result."""
-    T = 5
+    T = 1
     if np.ndim(M) == 0:
         M = np.ones_like(N)*M
     elif np.ndim(N) == 0:
@@ -86,17 +90,27 @@ def refinement(M,N,method,savename=False):
         assert(len(M)==len(N))
         
     err = np.zeros(len(M))
+    err_l2 = np.zeros(len(M))
     for i in range(len(M)):
-        x = np.linspace(-5,5,M[i]+2)
+        x = np.linspace(0,10,M[i]+2)
         t = np.linspace(0,T,N[i]+1) 
         U = num_solution(x,t,method)
         u = analytical_solution(x,t[-1])
         err[i] = e_l(U[-1,:],u)
+        err_l2[i] = la.norm(U[-1,:] - u)
+
+
+        plt.plot(x, U[-1, :], label = "num")
+        plt.plot(x, u, label = "an", linestyle = "--")
+        plt.legend()
+        plt.show()
 
     Ndof = M*N
     #Maybe we need more plots in the same figure?
+    plot_order(Ndof, err[0], 2, label=r"$\mathcal{O}(N_{dof}^{-2})$", color="green")
     plt.plot(Ndof, err, label=r"$e^r_{\ell}$", color='red', marker = 'o')
-    plot_order(Ndof, err[0], 2, label=r"$\mathcal{O}(N_{dof}^{-2})$", color="red")
+    plt.plot(Ndof, err_l2, label=r"$l2$", color='green', marker = 'o')
+    print(err_l2)
     plt.suptitle('RK4')
     
     plt.xscale('log')
@@ -109,6 +123,8 @@ def refinement(M,N,method,savename=False):
         plt.savefig(savename+".pdf")
     plt.show()
     
-M = np.array([8,16,32,64,128])
-N = 800
-#refinement(M,N,RK4_step)
+#M = np.array([8,16,32,64,128])
+#N = 800
+M = 50
+N = np.array([2**i for i in range(6, 10)])
+refinement(M,N,RK4_step)
